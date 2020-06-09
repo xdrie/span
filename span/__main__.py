f331cef67d2deb63a6cb86f8146535e14b5288d6
xdrie <xdrie@alt.icu>
2020-06-08 19:25:15 -0700

    migrate to typer

--- span/__main__.py
+++ span/__main__.py
@@ -3,38 +3,27 @@ from loguru import logger
 import settings
 from auth import authenticate_client
 from spotipy import Spotify
-import click
+import typer
 import jsonpickle
 import models
 
-# ensure args
-if not settings.SPOTIFY_USER:
-    logger.error("SPOTIFY_USER not set.")
-    sys.exit(1)
-
 
 def get_client() -> Spotify:  # get an authenticated client
+    if not settings.SPOTIFY_USER:  # ensure args
+        logger.error("SPOTIFY_USER not set.")
+        raise typer.Exit(1)
     return authenticate_client(settings.SPOTIFY_USER, settings.SPOTIFY_SCOPE)
 
 
-@click.group()
-@click.option("-v", "--verbose", is_flag=True)
-def cli(verbose):
-    """SPAN (SPotify-ANalyzer) liberates data from Spotify for local analysis"""
-    if verbose:
-        logger.remove()
-        logger.add(sys.stderr, level="TRACE")
-        logger.trace("verbose mode enabled")
+app = typer.Typer()
+fetch_app = typer.Typer()
+app.add_typer(fetch_app, name="fetch", help="Fetch remote lists via the Spotify API")
+analyze_app = typer.Typer()
+app.add_typer(analyze_app, name="analyze", help="Analyze data using the Spotify API")
 
 
-@cli.group()
-def fetch():
-    """Fetch remote lists via the Spotify API"""
-    pass
-
-
-@fetch.command()
-def liked():
+@fetch_app.command("liked")
+def fetch_liked():
     """Get liked songs (saved tracks in spotify library)"""
     sp = get_client()
 
@@ -46,14 +35,8 @@ def liked():
     sys.stdout.write(jsonpickle.encode(liked_tracks))
 
 
-@cli.group()
-def analyze():
-    """Analyze data using the Spotify API"""
-    pass
-
-
-@analyze.command()
-def features():
+@analyze_app.command("features")
+def analyze_features():
     """Analyze track features (read from stdin)"""
     sp = get_client()
 
@@ -64,9 +47,18 @@ def features():
     from tasks.features import get_audio_features
 
     features = get_audio_features(sp, tracks)
-    
+
     # export data
     sys.stdout.write(jsonpickle.encode(features))
 
 
-cli()
+@app.callback()
+def main(verbose: bool = typer.Option(False, "--verbose", "-v")):
+    """SPAN (SPotify-ANalyzer) liberates data from Spotify for local analysis"""
+    if verbose:
+        logger.remove()
+        logger.add(sys.stderr, level="TRACE")
+        logger.trace("verbose mode enabled")
+
+
+app()
